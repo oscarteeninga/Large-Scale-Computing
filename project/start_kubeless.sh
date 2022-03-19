0e7a139afef2a4602099d8a243b2a0e6e68c6573
Oscar Teeninga <o.teeninga@avsystem.com>
2022-03-19 10:21:31 +0100

    end

--- project/start_kubeless.sh
+++ project/start_kubeless.sh
@@ -13,13 +13,13 @@ brew install minikube
 minikube start -p aged --kubernetes-version=v1.16.1 --driver=hyperkit
 
 # Kubeless
-export RELEASE=$(curl -s https://api.github.com/repos/kubeless/kubeless/releases/latest | grep tag_name | cut -d '"' -f 4)
+export RELEASE=$(curl -s https://api.github.com/repos/vmware-archive/kubeless/releases/latest | grep tag_name | cut -d '"' -f 4)
 kubectl create ns kubeless
 kubectl create -f https://github.com/kubeless/kubeless/releases/download/$RELEASE/kubeless-$RELEASE.yaml
 kubectl get pods -n kubeless
 kubectl get deployment -n kubeless
 kubectl get customresourcedefinition
 export OS=$(uname -s| tr '[:upper:]' '[:lower:]')
-curl -OL https://github.com/kubeless/kubeless/releases/download/$RELEASE/kubeless_$OS-amd64.zip && \
+curl -OL https://github.com/vmware-archive/kubeless/releases/download/$RELEASE/kubeless_$OS-amd64.zip && \
   unzip kubeless_$OS-amd64.zip && \
   sudo mv bundles/kubeless_$OS-amd64/kubeless /usr/local/bin/
